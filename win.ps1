1553628c5c024bc94ebe279f597eeff080cee942
r0pchainz <38400850+r0pchainz@users.noreply.github.com>
2018-06-28 21:01:07 -0500

    Update win.ps1

--- win.ps1
+++ win.ps1
@@ -1,3 +1,4 @@
+#Requires -RunAsAdministrator
 reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /v SmartScreenEnabled /t REG_SZ /d "Off" /f
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\AppHost" /v "EnableWebContentEvaluation" /t REG_DWORD /d "0" /f
 reg add "HKCU\Software\Classes\Local Settings\Software\Microsoft\Windows\CurrentVersion\AppContainer\Storage\microsoft.microsoftedge_8wekyb3d8bbwe\MicrosoftEdge\PhishingFilter" /v "EnabledV9" /t REG_DWORD /d "0" /f
